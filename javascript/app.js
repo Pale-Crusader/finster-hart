c7a19a9ce8dfecb408d06a09411b792cde7a3a5f
MatthewStewart <matthew.adam.stewart@gmail.com>
2020-03-12 12:30:39 -0700

    succeed adding a form

--- javascript/app.js
+++ javascript/app.js
@@ -3,10 +3,13 @@ var loginFormEl = document.getElementById('loginForm');
 function handleForm(event) {
     event.preventDefault();
 
-    console.log(event);       
-    console.log(event.target.first.value);
-    console.log(event.target.last.value);
-    console.log(event.target.campaign.value);
+    // console.log(event +'just event');
+    // console.log(event.target +'just event & target');
+    // console.log(event.target.name +'just event & target & name');
+    // console.log(event.target.first +'just event & target first name');
+    console.log(event.target.first.value +'How Jacob makes it appear to show the value of imput with the name attribute of _first_');
+    console.log(event.target.last.value +'How Jacob makes it appear to show the value of imput with the name attribute of _last_');
+    console.log(event.target.campaign.value +'How Jacob makes it appear to show the value of imput with the name attribute of _campaign_');
 }
 
 
